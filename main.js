36637605b9fe88f625778f26bc50891b6662cc2c
jeong jun <jungjun3047@naver.com>
2022-03-17 23:10:11 +0900

    일단 fin

--- main.js
+++ main.js
@@ -1,32 +1,26 @@
 let target = document.querySelector("#dynamic");
-let orginStringArr;
 
 function randomSet(){
-let stringArr = ["Learn to HTML", "Learn to CSS", "Learn to JS"];
+let stringArr = ["Learn to HTML"];
 let selectString = stringArr[Math.floor(Math.random()* stringArr.length)];
 let selectStringArr = selectString.split("");
 orginStringArr = selectStringArr 
-console.log("randomSet : " + orginStringArr);
 return selectStringArr;
 }
 
 
-function typing(randomArr){
+function typing(randomArr){ 
     if(randomArr.length > 0){
-        console.log("typing : " + orginStringArr);
         target.textContent += randomArr.shift();
         setTimeout(function(){
             typing(randomArr);
         },80);
     }else{
-        setTimeout(reversedTyping(),3000)
+        setTimeout(resetTyping,3000);
     }
 }
 
-function reversedTyping(){
-}
-
-function resetTyping(){
+function resetTyping(){ 
     target.textContent = "";
     typing(randomSet());
 }
