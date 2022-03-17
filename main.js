4d877c662c7221cc14fef0cb54c34752b25e41b1
jeong jun <jungjun3047@naver.com>
2022-03-17 13:38:55 +0900

    wtf

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
 let target = document.querySelector("#dynamic");
+let orginStringArr;
 
-
-typing(randomString());
-setInterval(blink,500);
-
-function randomString(){
+function randomSet(){
 let stringArr = ["Learn to HTML", "Learn to CSS", "Learn to JS"];
-
 let selectString = stringArr[Math.floor(Math.random()* stringArr.length)];
-
 let selectStringArr = selectString.split("");
-
+orginStringArr = selectStringArr 
+console.log("randomSet : " + orginStringArr);
 return selectStringArr;
 }
 
-function resetTyping(){
-    target.textContent = "";
-    typing(randomString());
-}
 
 function typing(randomArr){
-    console.log(randomArr);
     if(randomArr.length > 0){
+        console.log("typing : " + orginStringArr);
         target.textContent += randomArr.shift();
         setTimeout(function(){
             typing(randomArr);
         },80);
     }else{
-        setTimeout(resetTyping,3000)
+        setTimeout(reversedTyping(),3000)
     }
 }
+
+function reversedTyping(){
+}
+
+function resetTyping(){
+    target.textContent = "";
+    typing(randomSet());
+}
+
+typing(randomSet());
+setInterval(blink,500);
 
 function blink(){
     target.classList.toggle("active");
