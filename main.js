759eaa1786a75fe429721ea0613e821a553f5512
jeong jun <jungjun3047@naver.com>
2022-03-17 09:55:26 +0900

    youtube example fin

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 let target = document.querySelector("#dynamic");
 
+
+typing(randomString());
+setInterval(blink,500);
+
+function randomString(){
 let stringArr = ["Learn to HTML", "Learn to CSS", "Learn to JS"];
 
 let selectString = stringArr[Math.floor(Math.random()* stringArr.length)];
 
-let selectStringArr = 
+let selectStringArr = selectString.split("");
+
+return selectStringArr;
+}
+
+function resetTyping(){
+    target.textContent = "";
+    typing(randomString());
+}
+
+function typing(randomArr){
+    console.log(randomArr);
+    if(randomArr.length > 0){
+        target.textContent += randomArr.shift();
+        setTimeout(function(){
+            typing(randomArr);
+        },80);
+    }else{
+        setTimeout(resetTyping,3000)
+    }
+}
 
 function blink(){
     target.classList.toggle("active");
 }
-setInterval(blink,500)
 
